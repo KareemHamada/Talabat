f24a97f42a45d493c9ba135694ab17509c194ce5
Kareem Hamada <81244276+KareemHamada@users.noreply.github.com>
2024-12-03 17:11:50 +0200

    first commit

---
+++ Talabat.Apis/Program.cs
@@ -0,0 +1,64 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Writers;
+using Talabat.Repository.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+#region Configure service
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StoreContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+#endregion
+
+
+
+var app = builder.Build();
+
+#region Update Database
+
+// Group of services lifetime scopped
+using var Scope = app.Services.CreateScope();
+// Services its self
+var Services = Scope.ServiceProvider;
+
+var LoggerFactory = Services.GetRequiredService<ILoggerFactory>();
+try
+{
+   
+    // Ask CLR for creating object from DbContext Explicitly
+    var DbContext = Services.GetRequiredService<StoreContext>();
+    // Update-database
+    await DbContext.Database.MigrateAsync();
+}catch(Exception ex)
+{
+    var Logger = LoggerFactory.CreateLogger<Program>();
+    Logger.LogError(ex,"An error occured during appling the migration");
+}
+
+//Scope.Dispose();
+#endregion
+
+
+#region Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+#endregion
+
+
+
+app.Run();
---
+++ Talabat.Repository/Data/Configurations/ProductConfigurations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Talabat.Core.Entities;
+
+namespace Talabat.Repository.Data.Configurations
+{
+    internal class ProductConfigurations : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasOne(product => product.ProductBrand)
+                .WithMany()
+                .HasForeignKey(p => p.ProductBrandId);
+                //.OnDelete(DeleteBehavior.SetNull);
+
+
+            builder.HasOne(product => product.ProductType)
+                .WithMany()
+                .HasForeignKey(p => p.ProductTypeId);
+
+
+
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Description).IsRequired();
+            builder.Property(p => p.PictureUrl).IsRequired();
+            builder.Property(p => p.Prcie).HasColumnType("decimal(18,2)");
+
+        }
+    }
+}
